912e6f89a692683ede750fc9550faa18a851079c
Sycrusian <sycrus@protonmail.com>
2023-08-07 17:24:09 -0300

    Atividade - Reduzindo Dados

--- script/script.js
+++ script/script.js
@@ -4,25 +4,25 @@
 const numbers = [20, 13, 50, 36, 97];
 
 function sumNumbers(array) {
-  return;
+  return array.reduce((sum, addend) => sum + addend);
 }
-// console.log(sumNumbers(numbers));
+console.log(sumNumbers(numbers));
 
 //2) Agora utilizaremos o nosso array de produtos. Crie uma função que percorra todos os elementos desse array e retorne o valor total de todos os produtos do carrinho.
 function totalProducts(array) {
-  return;
+  return array.reduce((total, product) => total + product.price, 0);
 }
-// console.log(totalProducts(products));
+console.log(totalProducts(products));
 
 //3) Vamos deixar mais interessante? Utilizando o nosso array products, crie uma função que filtre apenas os produtos com tamanho GG e retorne a soma dos preços desses produtos.
 function totalProductsSize(array) {
-  return;
+  return array.filter(product => product.size === "GG").reduce((total, product) => total + product.price, 0);
 }
-// console.log(totalProductsSize(products));
+console.log(totalProductsSize(products));
 
 //DESAFIO
 //4) Crie uma função que filtre os elementos em promoção e aplique 50% de desconto no preço deles. Após isso, some o valor total desses produtos.
 function totalProductsSale(array) {
-  return;
+  return array.filter(product => product.sale).map(product => product.price / 2).reduce((sum, addend) => sum + addend);
 }
-// console.log(totalProductsSale(products));
+console.log(totalProductsSale(products));
